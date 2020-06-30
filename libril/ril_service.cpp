62b141acccf359d00db090d577646b8fa6846017
Umashankar Godachi <umasha@codeaurora.org>
2020-06-30 12:24:43 +0530

    Remove local dynamic array
    Convert dynamic array in stack into vector
    and heap buffers.
    
    Change-Id: I4774de505f17f12e50a2e0bc963ecb8717c33cee
    CRs-Fixed: 2722172

--- libril/ril_service.cpp
+++ libril/ril_service.cpp
@@ -1798,8 +1798,12 @@ Return<void> RadioImpl::setGsmBroadcastConfig(int32_t serial,
     }
 
     int num = configInfo.size();
-    RIL_GSM_BroadcastSmsConfigInfo gsmBci[num];
-    RIL_GSM_BroadcastSmsConfigInfo *gsmBciPtrs[num];
+    auto gsmBci = new RIL_GSM_BroadcastSmsConfigInfo[num];
+    if (gsmBci == nullptr) {
+        sendErrorResponse(pRI, RIL_E_NO_MEMORY);
+        return Void();
+    }
+    std::vector<RIL_GSM_BroadcastSmsConfigInfo*> gsmBciPtrs(num);
 
     for (int i = 0 ; i < num ; i++ ) {
         gsmBciPtrs[i] = &gsmBci[i];
@@ -1810,8 +1814,10 @@ Return<void> RadioImpl::setGsmBroadcastConfig(int32_t serial,
         gsmBci[i].selected = BOOL_TO_INT(configInfo[i].selected);
     }
 
-    CALL_ONREQUEST(pRI->pCI->requestNumber, gsmBciPtrs,
+    CALL_ONREQUEST(pRI->pCI->requestNumber, gsmBciPtrs.data(),
             num * sizeof(RIL_GSM_BroadcastSmsConfigInfo *), pRI, mSlotId);
+
+    delete []gsmBci;
     return Void();
 }
 
@@ -1845,9 +1851,12 @@ Return<void> RadioImpl::setCdmaBroadcastConfig(int32_t serial,
     }
 
     int num = configInfo.size();
-    RIL_CDMA_BroadcastSmsConfigInfo cdmaBci[num];
-    RIL_CDMA_BroadcastSmsConfigInfo *cdmaBciPtrs[num];
-
+    auto cdmaBci = new RIL_CDMA_BroadcastSmsConfigInfo[num];
+    if (cdmaBci == nullptr) {
+        sendErrorResponse(pRI, RIL_E_NO_MEMORY);
+        return Void();
+    }
+    std::vector<RIL_CDMA_BroadcastSmsConfigInfo*> cdmaBciPtrs(num);
     for (int i = 0 ; i < num ; i++ ) {
         cdmaBciPtrs[i] = &cdmaBci[i];
         cdmaBci[i].service_category = configInfo[i].serviceCategory;
@@ -1855,8 +1864,10 @@ Return<void> RadioImpl::setCdmaBroadcastConfig(int32_t serial,
         cdmaBci[i].selected = BOOL_TO_INT(configInfo[i].selected);
     }
 
-    CALL_ONREQUEST(pRI->pCI->requestNumber, cdmaBciPtrs,
+    CALL_ONREQUEST(pRI->pCI->requestNumber, cdmaBciPtrs.data(),
             num * sizeof(RIL_CDMA_BroadcastSmsConfigInfo *), pRI, mSlotId);
+
+    delete []cdmaBci;
     return Void();
 }
 
